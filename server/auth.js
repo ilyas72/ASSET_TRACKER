b9bc6564ffafb7151538e54310359b15018ff9aa
ilyas72 <muhammadilyasjamil@gmail.com>
2018-11-09 10:28:10 +0800

    fourth version

---
+++ server/auth.js
@@ -0,0 +1,34 @@
+var jwt = require('express-jwt');
+
+function getTokenFromHeader(req){
+  console.log(req.path);
+  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token' ||
+      req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+    console.log(req.headers.authorization.split(' ')[1]);
+    return req.headers.authorization.split(' ')[1];
+  }
+  return null;
+}
+
+var getToken = function(req){
+  return getTokenFromHeader(req);
+}
+
+var auth = {
+  required: jwt({
+    //secret: process.env.JWT_SECRET,
+    secret: "ILOVEMADRASAHIRSYAD",
+    userProperty: 'payload',
+    getToken: getTokenFromHeader
+  }),
+  optional: jwt({
+    //secret: process.env.JWT_SECRET,
+    secret: "ILOVEMADRASAHIRSYAD",
+    userProperty: 'payload',
+    credentialsRequired: false,
+    getToken: getTokenFromHeader
+  }),
+  getToken: getToken
+};
+
+module.exports = auth;
--- server/main.js
+++ server/main.js
@@ -2,12 +2,19 @@
 const path = require('path');
 const express = require('express');
 const mysql = require('mysql');
+const auth = require('./auth');
 const bodyParser = require('body-parser');
 const multer = require ('multer');
 const multipart = multer ({dest: path.join(__dirname, "/uploads/")});
 const cors = require('cors');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 // const utils = require('./libs/mysql-utils');
 // const hbs = require('express-handlebars');
+const sqlFindUserByEmail = "SELECT * FROM USER WHERE email = ?";
+
 
 //Load MySQL and configure connection pool
 const pool = mysql.createPool(require('./dbconfig'));
@@ -45,6 +52,13 @@ var makeQuery = (sql, pool)=>{
     }
 }
 
+const sqlInsertUser = "INSERT INTO USER (email, password, name, salt) VALUES (?, ?, ?, ?)";
+var insertUser = makeQuery(sqlInsertUser, pool);
+
+
+var findUserByEmail = makeQuery(sqlFindUserByEmail, pool);
+
+
 //-----GET-----USERS-----//-----GET-----USERS-----//-----GET-----USERS-----//
 
 //Find all users
@@ -75,7 +89,48 @@ app.get("/user/:userId", (req, res)=>{
 })
 
 
+//app.post register
+app.post('/Register', bodyParser.urlencoded({ extended: true}), bodyParser.json({ limit: "50MB" }), (req, res)=>{
+    console.log("Post backend register 1");
+    let registerForm = req.body;
+    let registrationObj = {...registerForm};
+    console.log(JSON.stringify(registrationObj));
+    let convertSecObj = convertPasswordToHash(registrationObj.password);
+    registrationObj.password = convertSecObj.hash;
+    registrationObj.salt = convertSecObj.salt;
+    
+    insertUser([registrationObj.email, 
+        registrationObj.password, 
+        registrationObj.name,
+        registrationObj.salt]).then((results)=>{
+        console.log(results);
+        res.status(200).json({user: registrationObj});
+    }).catch((error)=>{
+        console.log(error);
+        res.status(500).json(error);
+    });
+})
 
+
+
+//convertPasswordToHash
+function convertPasswordToHash(password){
+    salt = crypto.randomBytes(Math.ceil(16/2))
+            .toString('hex') 
+            .slice(0,16); 
+    const key = crypto.pbkdf2Sync(password, salt, 100000, 64, 'sha512');
+    console.log("SALT 1> ", salt);
+    let hashObj = {
+        salt: salt,
+        hash: key.toString('hex')
+    }
+    return hashObj;
+}
+
+function isPasswordValid(password, currentHash, salt){
+    const key = crypto.pbkdf2Sync(password, salt, 100000, 64, 'sha512');
+    return key.toString('hex') === currentHash;
+}
 //-----GET-----ASSETS-----//-----GET-----ASSETS-----//-----GET-----ASSETS-----//
 
 //Find all assets 
@@ -155,23 +210,24 @@ app.get("/loan/asset/:assetId", (req, res)=>{
 
 //-----POST-----USERS-----//-----POST-----USERS-----//-----POST-----USERS-----//
 
-const sqlAddUser = 'INSERT INTO user (name, email, password) VALUES (?, ?, ?)';
-const addUser = makeQuery(sqlAddUser, pool);
-app.post("/user_reg",
-        bodyParser.urlencoded(),
-        bodyParser.json(),
-        (req, resp) => {
-            console.log("xxx in user_reg --> ", JSON.stringify(req.body));
-            addUser([req.body.name, req.body.email, req.body.password])
-                .then(() => {
-                    resp.status(201).json({ message: 'New user created'})
-                })
-                .catch(err => {
-                    console.log(err);
-                    resp.status(500).json({ error: err });
-                })
-        }
-    )
+// const sqlAddUser = 'INSERT INTO user (name, email, password) VALUES (?, ?, ?)';
+// const addUser = makeQuery(sqlAddUser, pool);
+// app.post("/Register",
+//         bodyParser.urlencoded(),
+//         bodyParser.json(),
+//         (req, resp) => {
+//             console.log("Post backend register 2");
+//             console.log("xxx in Register --> ", JSON.stringify(req.body));
+//             addUser([req.body.name, req.body.email, req.body.password])
+//                 .then(() => {
+//                     resp.status(201).json({ message: 'New user created'})
+//                 })
+//                 .catch(err => {
+//                     console.log(err);
+//                     resp.status(500).json({ error: err });
+//                 })
+//         }
+//     )
 
     const sqlModifyUser = 'UPDATE user SET  name = ?, email = ? WHERE user_id = ?';
     const modifyUser = makeQuery(sqlModifyUser, pool);
@@ -194,6 +250,107 @@ app.post("/user_reg",
             res.status(500).json(error);
         })
     })
+
+    // app.post('/Register', bodyParser.urlencoded({ extended: true}), bodyParser.json({ limit: "50MB" }), (req, res)=>{
+    //     console.log("Post backend register 3");
+    //     let registerForm = req.body;
+    //     let registrationObj = {...registerForm};
+    //     console.log(JSON.stringify(registrationObj));
+    //     let convertSecObj = convertPasswordToHash(registrationObj.password);
+    //     registrationObj.password = convertSecObj.hash;
+    //     registrationObj.salt = convertSecObj.salt;
+        
+    //     insertUser([registrationObj.email, 
+    //         registrationObj.password, 
+    //         registrationObj.fullName,
+    //         registrationObj.salt]).then((results)=>{
+    //         console.log(results);
+    //         res.status(200).json({user: registrationObj});
+    //     }).catch((error)=>{
+    //         console.log(error);
+    //         res.status(500).json(error);
+    //     });
+    // })    
+
+
+    // Change Password -> POST: /Change
+// Requires: user.id, user.newpassword, user.password
+// app.post('/Change', auth.required, bodyParser.urlencoded({ extended: true }), bodyParser.json({ limit: "50MB" }), (req, res) => {
+    app.post('/Change', bodyParser.urlencoded({ extended: true }), bodyParser.json({ limit: "50MB" }), (req, res) => {
+  
+    console.log("Post backend change password");
+    let changePasswordForm = req.body;
+    let changePasswordObj = { ...changePasswordForm };
+    console.log(JSON.stringify(changePasswordObj));
+    // Additional validation on server side
+
+    // Prevent empty password
+    if (changePasswordObj.newPassword.length == 0) {
+        console.log("Fail. New password cannot be empty.");
+        res.status(500).json({ result: "Fail. New password cannot be empty." });
+        return;
+    }
+    // Prevent empty password
+    if (changePasswordObj.password.length == 0) {
+        console.log("Fail. Current password cannot be empty.");
+        res.status(500).json({ result: "Fail. Current password cannot be empty." });
+        return;
+    }
+    const regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{12,}$/;
+    let m;
+    // Prevent invalid password
+    if ((m = regex.exec(changePasswordObj.password)) === null) {
+        console.log("Fail. Current password failed validation.");
+        res.status(500).json({ result: "Fail. Current password failed validation." });
+        return;
+    }
+    // Prevent invalid password
+    if ((m = regex.exec(changePasswordObj.newPassword)) === null) {
+        console.log("Fail. New password failed validation.");
+        res.status(500).json({ result: "Fail. New password failed validation." });
+        return;
+    }
+
+    let convertSecObj = convertPasswordToHash(changePasswordObj.newPassword);
+    changePasswordObj.newPassword = convertSecObj.hash;
+    changePasswordObj.salt = convertSecObj.salt;
+    
+    // Check old password is valid.
+    // To protect against the scenario where user is already logged in, but someone else use his account and tries to change password.
+    let oldPassword = changePasswordObj.password;
+    console.log("oldPassword =", oldPassword);
+    findUserByEmail([req.payload.username]).then((result) => {
+        if (result.length > 0) {
+            if (isPasswordValid(oldPassword, result[0].password, result[0].salt)) {
+                console.log("ITS MATCH !");
+                // Set the new password to password field.
+                changePasswordObj.password = changePasswordObj.newPassword;
+                // "UPDATE user SET password = ? , salt = ? WHERE id = ?"
+                updateUserPassword([changePasswordObj.password,
+                changePasswordObj.salt,
+                req.payload.username]).then((results) => {
+                    console.log("Success! results =>", results);
+                    res.status(200).json({ result: "Success!" });
+                }).catch((error) => {
+                    console.log("Error! error =>", error);
+                    res.status(500).json(error);
+                });
+            } else {
+                console.log("Fail. Current password incorrect. result =>", result);
+                res.status(500).json({ result: "Fail. Current password incorrect." });
+                return;
+            }
+        } else {
+            console.log("Fail. No record found. result =>", result);
+            res.status(500).json({ result: "Fail. No record found." });
+            return;
+        }
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).json(error);
+        return;
+    })
+})
 
 
 //-----//-----//-----//-----//-----//-----//-----//-----//-----//-----//-----//-----//  
@@ -220,6 +377,67 @@ pool.getConnection((err, conn) => {
         });
     });
 });
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  }, function(email, password, done) {
+      findUserByEmail([email]).then((result)=>{
+          if(result.length > 0){
+              if(isPasswordValid(password, result[0].password, result[0].salt)){
+                  console.log("ITS MATCH !");
+                  return done(null, result[0]);
+              }else{
+                  return done(null, false, {errors: {'email or password': 'is invalid'}});
+              }
+          }else{
+              return done(null, false, {errors: {'email or password': 'is invalid'}});
+          }
+      }).catch(done)
+    
+  }));
+
+//-----//-----//-----//-----//-----//-----//-----//-----//-----//-----//-----//-----//  
+//login
+
+  app.post('/Login', bodyParser.urlencoded({ extended: true}), bodyParser.json({ limit: "50MB" }), (req, res, next)=>{
+    let user = {...req.body};
+    let email = user.email;
+    let password = user.password;
+    console.log ("inLogin")
+
+    if(!email){
+        return res.status(422).json({errors: {email: "can't be blank"}});
+      }
+    
+    if(!password){
+    return res.status(422).json({errors: {password: "can't be blank"}});
+    }
+    
+    passport.authenticate('local', {session: false}, function(err, user, info){
+        if(err){ return next(err); }
+        var today = new Date();
+        var exp = new Date(today);
+        exp.setDate(today.getDate() + 60);
+        let token = jwt.sign({
+            id: user.user_id,
+            username: user.email,
+            exp: parseInt(exp.getTime() / 1000),
+        //}, process.env.JWT_SECRET);
+        }, "ILOVEMADRASAHIRSYAD");
+        
+        if(user){
+          console.log("JWT token > ", token);
+          user.token = token;
+          return res.json({user: user});
+        } else {
+          return res.status(422).json(info);
+        }
+      })(req, res, next);
+    
+})
+
+
 
 // port and server
 // const PORT = parseInt(process.argv[2]) || 
